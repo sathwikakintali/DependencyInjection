3ab96e11228c554985e7e0111b1e500ff312d861
Sathwika Kintali <Sathwika.Kintali@godigit.com>
2024-08-22 12:09:38 +0530

    dependency injection practice

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class Car {
+    private Engine engine;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "engine=" + engine +
+                '}';
+    }
+}
